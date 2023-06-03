f269d204b850654a5fdaf8fba85742f9e5f4c4c8
Diego <daugsteel@gmail.com>
2023-06-03 17:00:15 -0300

    Fixing Images

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default class Home extends Component {
               <li key={k}>
                 <h2>{cat.name}</h2>
                 <p>{cat.description}</p>
-                <img src={cat.photo} alt={cat.name}/>
+                <img src={} alt={cat.name}/>
               </li>
             ))}
           </ul>
