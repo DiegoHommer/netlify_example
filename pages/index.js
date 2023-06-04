5887181d1deeb627a81265b8e88bdbfbf68bfa41
Diego <daugsteel@gmail.com>
2023-06-04 00:36:05 -0300

    Dynamic MDs

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,47 @@
-import Head from "next/head"
-import { Component } from 'react'
-import { attributes, react as HomeContent } from '../content/home.md'
-import styles from "./Home.module.css"
+import fs from 'fs'
+import matter from 'gray-matter'
+import Link from 'next/link'
+import Head from 'next/head'
+import styles from '../Home.module.css'
 
-export default class Home extends Component {
-  render() {
-    let { title, cats } = attributes
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <article className={styles.container}>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul> 
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-                <img className={styles.imageContainer} src={cat.photo} alt={cat.name}/>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </>
-    )
-  }
+export default function Home({ blogs }) {
+  return (<div className={styles['container']}>
+    <Head>
+      <title>Demo Blog</title>
+    </Head>
+    <h1 className={styles['header']}>Welcome to my blog</h1>
+    <p className={styles['subtitle']}>This is a subtitle idk what to type here</p>
+    <ul className={styles['blog-list']}>
+      {blogs.map(blog => (
+        <li key={blog.slug}>
+          <Link href={`/blog/${blog.slug}`}>
+            <a>{blog.date}:{blog.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>)
 }
 
+export async function getStaticProps() {
+  // List of files in blgos folder
+  const filesInBlogs = fs.readdirSync('./content')
+
+  // Get the front matter and slug (the filename without .md) of all files
+  const blogs = filesInBlogs.map(filename => {
+    const file = fs.readFileSync(`./content/${filename}`, 'utf8')
+    const matterData = matter(file)
+
+    return {
+      ...matterData.data, // matterData.data contains front matter
+      slug: filename.slice(0, filename.indexOf('.'))
+    }
+  })
+
+  return {
+    props: {
+      blogs
+    }
+  }
+
+}
