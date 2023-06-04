979d67736d158b98ba6b5d75fc1e01b3e048ee42
Diego <daugsteel@gmail.com>
2023-06-04 00:53:02 -0300

    Testing Dynamic

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ import Link from 'next/link'
 import Head from 'next/head'
 import styles from './Home.module.css'
 
-export default function Home({ blogs }) {
+export default function Home({ cats }) {
   return (<div className={styles['container']}>
     <Head>
-      <title>Demo Blog</title>
+      <title>Demo </title>
     </Head>
     <h1 className={styles['header']}>Welcome to my blog</h1>
     <p className={styles['subtitle']}>This is a subtitle idk what to type here</p>
     <ul className={styles['blog-list']}>
-      {blogs.map(blog => (
-        <li key={blog.slug}>
-          <Link href={`/blog/${blog.slug}`}>
-            <a>{blog.date}:{blog.title}</a>
-          </Link>
-        </li>
+      {cats.map(cat => (
+        <div>
+          <h1> {cat.name} </h1>
+          <h2> {cat.description} </h2>
+          <img src={cat.photo} alt={cat.name}/>
+        </div>
       ))}
     </ul>
   </div>)
@@ -25,10 +25,10 @@ export default function Home({ blogs }) {
 
 export async function getStaticProps() {
   // List of files in blgos folder
-  const filesInBlogs = fs.readdirSync('./content')
+  const filesInCats = fs.readdirSync('./content')
 
   // Get the front matter and slug (the filename without .md) of all files
-  const blogs = filesInBlogs.map(filename => {
+  const cats = filesInCats.map(filename => {
     const file = fs.readFileSync(`./content/${filename}`, 'utf8')
     const matterData = matter(file)
 
@@ -40,7 +40,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      blogs
+      cats
     }
   }
 
