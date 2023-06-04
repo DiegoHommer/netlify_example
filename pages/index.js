ba0009751743db7bc54e38e44585f6d1f51f6a65
Diego <daugsteel@gmail.com>
2023-06-04 12:28:47 -0300

    Improving MDs

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,19 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import Link from 'next/link'
 import Head from 'next/head'
-import styles from './Home.module.css'
+import styles from './Styles/Home.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default function Home({ recipes }) {
-  return (<div className={styles['container']}>
+export default function Home() {
+
+  return (
+  <div className={styles['container']}>
     <Head>
-      <title>Demo </title>
+      <title>IDE's Culinary Site</title>
     </Head>
     <h1 className={styles['header']}>Cooking Recipes</h1>
-    <p className={styles['subtitle']}>Home made recipes available right here!</p>
-    <ul className={styles['blog-list']}>
-      {recipes.map(recipe => (
-        <div>
-          <h1> {recipe.title} </h1>
-          <h2> {recipe.date} </h2>
-          <h3> {recipe.body} </h3>
-          <img src={recipe.photo} alt={recipe.title}/>
-        </div>
-      ))}
-    </ul>
+    <p className={styles['subtitle']}>Home made recipes available right 
+      <Link href="/recipes"> HERE </Link>
+    </p>
   </div>)
 }
 
-export async function getStaticProps() {
-  // List of files in blgos folder
-  const filesInRecipes = fs.readdirSync('./content/recipes')
-
-  // Get the front matter and slug (the filename without .md) of all files
-  const recipes = filesInRecipes.map(filename => {
-    const file = fs.readFileSync(`./content/recipes/${filename}`, 'utf8')
-    const matterData = matter(file)
-
-    return {
-      ...matterData.data, // matterData.data contains front matter
-      slug: filename.slice(0, filename.indexOf('.'))
-    }
-  })
-
-  return {
-    props: {
-      recipes
-    }
-  }
-
-}
---
+++ pages/recipes.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import styles from './Styles/Recipes.module.css'
+
+export default function Recipes({ recipes }) {
+    return(
+        <ul className={styles['recipe-list']}>
+        {recipes.map(recipe => (
+            <div>
+              <h1> {recipe.title} </h1>
+              <h2> {recipe.date} </h2>
+              <img src={recipe.photo} alt={recipe.title}/>
+              <h3> {recipe.body} </h3>
+            </div>
+        ))}
+        </ul>
+    )
+}
+
+export async function getStaticProps() {
+    // List of files in recipes folder
+    const filesInRecipes = fs.readdirSync('./content/recipes')
+  
+    // Get the front matter and slug (the filename without .md) of all files
+    const recipes = filesInRecipes.map(filename => {
+      const file = fs.readFileSync(`./content/recipes/${filename}`, 'utf8')
+      const matterData = matter(file)
+  
+      return {
+        ...matterData.data, // matterData.data contains front matter
+        slug: filename.slice(0, filename.indexOf('.'))
+      }
+    })
+  
+    return {
+      props: {
+        recipes
+      }
+    }
+  
+  }
